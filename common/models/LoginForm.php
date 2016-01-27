6e9ce3ebe3af2376195f718ab1759676116eb9bb
proskurin <proskurin@proskurin.local>
2016-01-27 13:53:39 +0300

    переустановка composer добавлен yiisoft/yii2-jui: "*"

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -13,7 +13,7 @@ class LoginForm extends Model
 {
     public $username;
     public $password;
-    public $rememberMe = true;
+    public $rememberMe = false;
 
     private $_user = false;
 
@@ -45,7 +45,7 @@ class LoginForm extends Model
         if (!$this->hasErrors()) {
             $user = $this->getUser();
             if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Имя пользователя или пароль не правильные.');
+                $this->addError($attribute, 'Incorrect username or password.');
             }
         }
     }
--- frontend/views/profile/_form.php
+++ frontend/views/profile/_form.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use yii\jui\DatePicker;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Profile */
 /* @var $form yii\widgets\ActiveForm */
