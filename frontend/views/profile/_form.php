035eeeb5aee7eb9edb4f640d89acd58335a787fa
proskurin <proskurin@proskurin.local>
2016-01-27 10:50:27 +0300

    Внесены изменения. Добавлен выпадающий список, позволяющий выбрать ПОЛ

--- frontend/views/profile/_form.php
+++ frontend/views/profile/_form.php
@@ -11,23 +11,17 @@ use yii\widgets\ActiveForm;
 <div class="profile-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <?= $form->field($model, 'first_name')->textInput(['maxlength' => 45])?>
+    <?= $form->field($model, 'last_name')->textInput(['maxlength' => 45])?>
+    <br>
+        <?= $form->field($model, 'birthdate')->textInput()?>
+    <br>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
-
-    <?= $form->field($model, 'first_name')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'last_name')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'birthdate')->textInput() ?>
-
-    <?= $form->field($model, 'gender_id')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+    <?= $form->field($model, 'gender_id')->dropDownList($model->genderList, ['promt'=> 'Выберите пол']);?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update',
+            ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
